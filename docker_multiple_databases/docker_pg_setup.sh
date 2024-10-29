7c6f247bcd9c144aa099141db3c5e31f786fb799
Aleksandr Ushakov <aleksandr.ushakov@vk.team>
2024-10-29 11:07:57 +0300

    fix backend keycloak scripts

--- docker_multiple_databases/docker_pg_setup.sh
+++ docker_multiple_databases/docker_pg_setup.sh
@@ -7,6 +7,5 @@ set -x
 echo "Start database restoration"
 
 psql keycloak_db -U "$POSTGRES_USER" -f /docker-entrypoint-initdb.d/dump.sql
-#pg_restore -p 5432 -U "$POSTGRES_USER" -v -d keycloak_db /docker-entrypoint-initdb.d/dump.sql
 
 set +x
