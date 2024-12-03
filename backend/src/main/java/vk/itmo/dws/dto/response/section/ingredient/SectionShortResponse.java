b72feb587314d99a836bc07565ab95e57ce9c82c
IDante <maxim.polyanskiy@mail.ru>
2024-12-03 13:15:41 +0300

    fix: sections response

--- backend/src/main/java/vk/itmo/dws/dto/response/section/ingredient/SectionShortResponse.java
+++ backend/src/main/java/vk/itmo/dws/dto/response/section/ingredient/SectionShortResponse.java
@@ -3,8 +3,12 @@ package vk.itmo.dws.dto.response.section.ingredient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import vk.itmo.dws.dto.response.classes.ClassResponse;
+import vk.itmo.dws.entity.Class;
+import vk.itmo.dws.entity.Section;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +16,13 @@ import java.time.LocalDate;
 public class SectionShortResponse {
     protected Long id;
     protected String title;
+    protected List<ClassResponse> classes;
+    protected String description;
+
+    public SectionShortResponse(Section section){
+        this.id = section.getId();
+        this.title = section.getTitle();
+        this.classes = section.getClasses().stream().map(ClassResponse::new).toList();
+        this.description = section.getDescription();
+    }
 }
